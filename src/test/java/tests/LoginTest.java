2bcb62b60070faf6312035c277b2b8259a3f983b
SDETMatomeMasiye <masiyeholdings@gmail.com>
2026-06-29 19:28:43 +0200

    Updating to run in headless mode

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -22,7 +22,7 @@ class LoginTest {
     @BeforeAll
     static void setupForAll(){
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(List.of("--start-maximized")));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setArgs(List.of("--start-maximized")));
     }
 
     @AfterAll
